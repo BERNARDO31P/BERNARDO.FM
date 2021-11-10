e2a950d49e862dcd6238cbb2bc6ecdaa808819ed
bernardo <bernardo@bernardo.fm>
2021-11-10 14:48:55 +0100

    Implemented monitoring

--- assets/js/pages/monitoring.js
+++ assets/js/pages/monitoring.js
@@ -1,123 +1,153 @@
+let httpGetM = await import(pageURL + "assets/js/httpGet.js");
+let tryParseJSONM = await import(pageURL + "assets/js/tryParseJSON.js");
+
 window["monitoring"] = () => {
-    let content = document.getElementById("content");
-    let canvas = document.createElement("canvas");
-    canvas.classList.add("graph");
-
-    content.insertBefore(canvas, content.firstChild);
-
-    let testValues = [0, 6, 8, 7, 5, 6, 5, 10, 2, 13, 1, 5, 9, 14];
-    let testTime = ["00:00", "00:20", "00:40", "01:00", "01:20", "01:40", "02:00"];
+    let canvasDown = document.getElementById("download"), canvasUp = document.getElementById("upload"),
+        canvasCpu = document.getElementById("cpu"), canvasRam = document.getElementById("ram");
 
     setInterval(function () {
-        let lastTime = testTime.at(-1);
-        let parts = lastTime.split(":");
-        let date = new Date(null, null, null, Number(parts[0]), Number(parts[1]));
-        let newTime = addMinutes(date, 20);
+        let data = tryParseJSONM.tryParseJSON(httpGetM.httpGet("/db/monitoring.json"));
+        let timestamps = Object.keys(data);
+        let timeValues = [...new Set(timestampToTime(timestamps))];
 
-        testValues.push(Math.random() * (1000 - 1) + 1);
-        testTime.push(("0" + newTime.getHours()).slice(-2) + ":" + ("0" + newTime.getMinutes()).slice(-2));
-        testValues.shift();
-        testTime.shift();
+        let downValues = Object.values(data).map(function(d) { return d["network"]["down"]; });
+        let upValues = Object.values(data).map(function(d) { return d["network"]["up"]; });
+        let cpuValues = Object.values(data).map(function(d) { return d["cpu"]; });
+        let ramValues = Object.values(data).map(function(d) { return d["ram"]; });
 
-        drawGraph(canvas, testValues, testTime);
-    }, 1000);
+        drawGraph(canvasDown, downValues, timeValues, "Mbit/s");
+        drawGraph(canvasUp, upValues, timeValues, "Mbit/s");
+        drawGraph(canvasCpu, cpuValues, timeValues, "%");
+        drawGraph(canvasRam, ramValues, timeValues, "%");
+    }, 2000);
 }
 
-function addMinutes(date, minutes) {
-    return new Date(date.getTime() + minutes * 60000);
+/*
+ * Funktion: timestampToTime()
+ * Autor: Bernardo de Oliveira
+ * Argumente:
+ *  timestamps: (Array) Die Timestamps welche umgewandelt werden sollen
+ *
+ * Konvertiert Timestamps zu einer leserlichen Zeit um
+ */
+function timestampToTime(timestamps) {
+    let time = [];
+
+    for (let timestamp of timestamps) {
+        let date = new Date(timestamp * 1000);
+        let hours = ("0" + date.getHours()).substr(-2);
+        let minutes = ("0" + date.getMinutes()).substr(-2);
+
+        time.push(hours + ":" + minutes);
+    }
+
+    return time;
 }
 
-
-function drawGraph(canvas, dataArr, timeArr) {
+/*
+ * Funktion: drawGraph()
+ * Autor: Bernardo de Oliveira
+ * Argumente:
+ *  canvas: (Objekt) Die Timestamps welche umgewandelt werden sollen
+ *  dataArr: (Array) Die Werte
+ *  timeArr: (Array) Die Uhrzeiten
+ *  measurement: (String) Die Masseinheit der Werte
+ *
+ * Zeichnet einen Graphen anhand von Werten und Zeiten
+ * !Inline-Kommentare beachten!
+ */
+function drawGraph(canvas, dataArr, timeArr, measurement) {
     let context = canvas.getContext("2d");
     let canvasStyle = window.getComputedStyle(canvas);
     let canvasWidth = Number(canvasStyle.width.replace("px", ""));
     let canvasHeight = Number(canvasStyle.height.replace("px", ""));
 
-    let GRAPH_HEIGHT = canvasHeight - 100;
-    let GRAPH_WIDTH = canvasWidth - 100;
-
+    // Hiermit passt sich der Graph an
+    let GRAPH_HEIGHT = canvasHeight;
+    let GRAPH_WIDTH = canvasWidth;
     canvas.height = canvasHeight;
     canvas.width = canvasWidth;
 
-    let GRAPH_TOP = 25;
-    let GRAPH_BOTTOM = GRAPH_HEIGHT + GRAPH_TOP;
-    let GRAPH_LEFT = 35;
-    let GRAPH_RIGHT = GRAPH_WIDTH - GRAPH_LEFT;
+    // Definition von den Rändern
+    let GRAPH_TOP = 30;
+    let GRAPH_BOTTOM = GRAPH_HEIGHT - (GRAPH_TOP * 2);
+    let GRAPH_LEFT = 20;
+    let GRAPH_RIGHT = GRAPH_WIDTH - (GRAPH_LEFT * 5);
 
     let arrayLen = dataArr.length;
     let largest = Math.max(...dataArr);
+    let smallest = Math.min(...dataArr);
 
+    // Graph-Clear
     context.clearRect(0, 0, canvasWidth, canvasHeight);
-    // set font for fillText()
-    context.font = "16px Arial";
+    // Setze Schriftart für fillText()
+    context.font = "13px Arial";
 
-    // draw X and Y axis
+    // Umriss Generierung
     context.beginPath();
     context.moveTo(GRAPH_LEFT, GRAPH_BOTTOM);
     context.lineTo(GRAPH_RIGHT, GRAPH_BOTTOM);
     context.lineTo(GRAPH_RIGHT, GRAPH_TOP);
     context.stroke();
 
-    // draw reference line
+    // Referenzlinien zeichnen
     context.beginPath();
     context.strokeStyle = "#BBB";
     context.moveTo(GRAPH_LEFT, GRAPH_TOP);
     context.lineTo(GRAPH_RIGHT, GRAPH_TOP);
-    // draw reference value for hours
+    // Referenz für die Daten: Erster Wert
     context.fillText(Math.round(largest * 100) / 100, GRAPH_RIGHT + 15, GRAPH_TOP);
     context.stroke();
 
-    // draw reference line
-    context.beginPath();
-    context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
-    context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
-    // draw reference value for hours
-    context.fillText(Math.round(largest / 4 * 100) / 100, GRAPH_RIGHT + 15, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
-    context.stroke();
-
-    // draw reference line
-    context.beginPath();
-    context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
-    context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
-    // draw reference value for hours
-    context.fillText(Math.round(largest / 2 * 100) / 100 + " Mbit/s", GRAPH_RIGHT + 15, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
-    context.stroke();
-
-    // draw reference line)
+    // Referenzlinien zeichnen
     context.beginPath();
     context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 4 + GRAPH_TOP);
     context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 4 + GRAPH_TOP);
-    // draw reference value for hours
-    context.fillText(Math.round(largest / 4 * 3 * 100) / 100, GRAPH_RIGHT + 15, (GRAPH_HEIGHT) / 4 + GRAPH_TOP);
+    // Referenz für die Daten: Zweiter Wert
+    context.fillText(Math.round((smallest + ((largest - smallest) / 3) * 2) * 100) / 100, GRAPH_RIGHT + 15, (GRAPH_HEIGHT) / 4 + GRAPH_TOP);
     context.stroke();
 
-    // draw titles
-    context.fillText("Time", GRAPH_RIGHT / 2.23, GRAPH_BOTTOM + 50);
+    // Referenzlinien zeichnen
+    context.beginPath();
+    context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
+    context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
+    // Referenz für die Daten: Dritter Wert
+    context.fillText(Math.round((smallest + (largest - smallest) / 3) * 100) / 100 + " " + measurement, GRAPH_RIGHT + 15, (GRAPH_HEIGHT) / 2 + GRAPH_TOP);
+    context.stroke();
 
+    // Referenzlinien zeichnen
+    context.beginPath();
+    context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
+    context.lineTo(GRAPH_RIGHT, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
+    // Referenz für die Daten: Letzter Wert
+    context.fillText(Math.round(smallest * 100) / 100, GRAPH_RIGHT + 15, (GRAPH_HEIGHT) / 4 * 3 + GRAPH_TOP);
+    context.stroke();
+
+    // Information, dass die Werte Uhrzeiten sind
+    context.fillText("Time", GRAPH_RIGHT / 2 + GRAPH_LEFT, GRAPH_BOTTOM + 50);
+
+    // Referenz für die Zeit
+    for (let i = 0, j = 0; i < arrayLen; i++) {
+        if (i % 30 === 0) {
+            context.fillText(timeArr[j], GRAPH_RIGHT / arrayLen * i + GRAPH_LEFT, GRAPH_BOTTOM + GRAPH_TOP);
+            j++;
+        }
+    }
+
+    // Verbindungslinie Zeichnen
     context.beginPath();
     context.fillStyle = "black";
     context.strokeStyle = "black";
     context.lineWidth = 2;
-
-    context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT - dataArr[0] / largest * GRAPH_HEIGHT) + GRAPH_TOP);
-    // draw reference value for day of the week
-    for (let i = 0, j = 0; i < arrayLen; i++) {
-        context.lineTo(GRAPH_RIGHT / arrayLen * i + GRAPH_LEFT, (GRAPH_HEIGHT - dataArr[i] / largest * GRAPH_HEIGHT) + GRAPH_TOP);
-        // draw reference value for day of the week
-
-        if (i % 4 === 0) {
-            context.fillText(timeArr[j], GRAPH_RIGHT / arrayLen * i + 15, GRAPH_BOTTOM + GRAPH_TOP);
-            j++;
-        }
+    for (let i = 0; i < arrayLen; i++) {
+        context.lineTo((GRAPH_RIGHT - GRAPH_LEFT) / arrayLen * i + GRAPH_LEFT, ((GRAPH_BOTTOM - GRAPH_TOP) - dataArr[i] / largest * (GRAPH_BOTTOM - GRAPH_TOP)) + GRAPH_TOP);
     }
     context.stroke();
 
-    // drawing of circles
-    context.moveTo(GRAPH_LEFT, (GRAPH_HEIGHT - dataArr[0] / largest * GRAPH_HEIGHT) + GRAPH_TOP);
+    // Punkte zeichnen
     for (let i = 0; i < arrayLen; i++) {
         const circle = new Path2D();
-        circle.arc(GRAPH_RIGHT / arrayLen * i + GRAPH_LEFT, (GRAPH_HEIGHT - dataArr[i] / largest * GRAPH_HEIGHT) + GRAPH_TOP, 6, 0, 2 * Math.PI);
+        circle.arc((GRAPH_RIGHT - GRAPH_LEFT) / arrayLen * i + GRAPH_LEFT, ((GRAPH_BOTTOM - GRAPH_TOP) - dataArr[i] / largest * (GRAPH_BOTTOM - GRAPH_TOP)) + GRAPH_TOP, 3, 0, 2 * Math.PI);
         context.fillStyle = 'black';
         context.fill(circle);
     }
--- system/monitoring.php
+++ system/monitoring.php
@@ -1,4 +1,8 @@
 <?php
+$amount = 30;
+$db = json_decode(file_get_contents(__DIR__ . "/../db/monitoring.json"), true) ?? array();
+$db = array_slice($db, -($amount * 4), $amount * 4, true);
+
 function get_server_memory_usage() {
     $free = shell_exec('free');
     $free = (string)trim($free);
@@ -6,28 +10,52 @@ function get_server_memory_usage() {
     $mem = explode(" ", $free_arr[1]);
     $mem = array_filter($mem);
     $mem = array_merge($mem);
-    return $mem[2]/$mem[1]*100;
+    return round($mem[2]/$mem[1]*100, 2);
 }
 
 function get_server_cpu_usage() {
-    $load = sys_getloadavg();
-    return $load[0];
-}
-
-function get_server_network_usage() {
-    $int="eth0";
-
-    $rx[] = @file_get_contents("/sys/class/net/$int/statistics/rx_bytes");
-    $tx[] = @file_get_contents("/sys/class/net/$int/statistics/tx_bytes");
+    $cont = file('/proc/stat');
+    $cpuloadtmp = explode(' ',$cont[0]);
+    $cpuload0[0] = $cpuloadtmp[2] + $cpuloadtmp[4];
+    $cpuload0[1] = $cpuloadtmp[2] + $cpuloadtmp[4]+ $cpuloadtmp[5];
     sleep(1);
-    $rx[] = @file_get_contents("/sys/class/net/$int/statistics/rx_bytes");
-    $tx[] = @file_get_contents("/sys/class/net/$int/statistics/tx_bytes");
-
-    $tbps = $tx[1] - $tx[0];
-    $rbps = $rx[1] - $rx[0];
-
-    $round_rx=round($rbps/1024, 2);
-    $round_tx=round($tbps/1024, 2);
+    $cont = file('/proc/stat');
+    $cpuloadtmp = explode(' ',$cont[0]);
+    $cpuload1[0] = $cpuloadtmp[2] + $cpuloadtmp[4];
+    $cpuload1[1] = $cpuloadtmp[2] + $cpuloadtmp[4]+ $cpuloadtmp[5];
+    return round(($cpuload1[0] - $cpuload0[0])*100/($cpuload1[1] - $cpuload0[1]), 2);
 }
 
-echo get_server_cpu_usage();
+function get_server_network_usage(): array
+{
+    $int="wlp5s0";
+
+    $rx[] = file_get_contents("/sys/class/net/$int/statistics/rx_bytes");
+    $tx[] = file_get_contents("/sys/class/net/$int/statistics/tx_bytes");
+    sleep(1);
+    $rx[] = file_get_contents("/sys/class/net/$int/statistics/rx_bytes");
+    $tx[] = file_get_contents("/sys/class/net/$int/statistics/tx_bytes");
+
+    $tbps = intval($tx[1]) - intval($tx[0]);
+    $rbps = intval($rx[1]) - intval($rx[0]);
+
+    $round_rx=round($rbps/1000000, 2);
+    $round_tx=round($tbps/1000000, 2);
+
+    return ["down" => $round_rx * 8, "up" => $round_tx * 8];
+}
+
+while(true) {
+    if (count($db) == $amount * 4) {
+        reset($db);
+        unset($db[key($db)]);
+    }
+
+    $db[time()] = array(
+        "cpu" => get_server_cpu_usage(),
+        "ram" => get_server_memory_usage(),
+        "network" => get_server_network_usage()
+    );
+
+    file_put_contents(__DIR__ . "/../db/monitoring.json", json_encode($db));
+}
