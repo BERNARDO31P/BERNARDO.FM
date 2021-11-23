618f847f50153843b9e13d86d715e71df507506b
bernardo <bernardo@bernardo.fm>
2021-11-23 14:49:18 +0100

    Parsing of the firewall implemented

--- system/firewall.php
+++ system/firewall.php
@@ -1,12 +1,29 @@
 <?php
 
-// TODO: Comment
+/*
+ * Funktion: myFilter()
+ * Autor: Bernardo de Oliveira
+ * Argumente:
+ *  var: (Objekt) Definiert das zu überprüfende Element
+ *
+ * Eigener Filter für array_filter
+ * Überprüft, ob das Element leer ist
+ */
 function myFilter($var): bool
 {
     return ($var !== NULL && $var !== FALSE && $var !== '');
 }
 
-// TODO: Comment
+/*
+ * Funktion: specialSplit()
+ * Autor: Bernardo de Oliveira & lonesomeday (https://stackoverflow.com/a/4538153)
+ * Argumente:
+ *  string: (String) Definiert den Text, welcher geteilt werden soll
+ *
+ * Da str_split() keine Zusammenhänge erkennt und nur ein Zeichen beachtet, wurde diese Funktion programmiert
+ * Funktioniert wie str_split(), teilt bei jedem Leerzeichen, beachtet aber zusammenhängende Zeichen
+ * Teilt nicht, wenn die Zeichenfolge sich zwischen Klammern () oder zwischen \/**\/ befindet (ohne \).
+ */
 function specialSplit($string): array
 {
     $level = 0;
@@ -43,7 +60,14 @@ function specialSplit($string): array
     return $ret;
 }
 
-// TODO: Comment
+/*
+ * Funktion: clearArray()
+ * Autor: Bernardo de Oliveira
+ * Argumente:
+ *  array: (Array) Definiert das Array welches bereinigt werden soll
+ *
+ * Entfernt rekursiv alle leeren Elemente aus dem Array
+ */
 function clearArray($array): array
 {
     foreach ($array as $arrID => $value) {
@@ -57,10 +81,17 @@ function clearArray($array): array
     return array_filter($array);
 }
 
-// TODO: Comment
-function sort_recursive(&$array, $flags = null) {
+/*
+ * Funktion: sort_recursive()
+ * Autor: Bernardo de Oliveira
+ * Argumente:
+ *  array: (Array) Definiert das Array welches sortiert werden soll
+ *
+ * Definiert die Array-Schlüssel neu, wenn diese aus Zahlen bestehen
+ */
+function sort_recursive(&$array) {
     foreach ($array as &$value) {
-        if (is_array($value)) sort_recursive($value, $flags);
+        if (is_array($value)) sort_recursive($value);
     }
     unset($value);
 
@@ -76,7 +107,18 @@ function sort_recursive(&$array, $flags = null) {
     }
 }
 
-// TODO: Comment
+/*
+ * Funktion: structureArray()
+ * Autor: Bernardo de Oliveira
+ * Argumente:
+ *  array: (Array) Definiert das Array, welches neu strukturiert werden soll
+ *
+ * Findet heraus, welche Zeile die Array-Schlüssel beinhaltet
+ * Ändert die nummerischen Array-Schlüssel von den Werten um zu den gefundenen Text-Schlüssel
+ * Beispiel:
+ *  Aus dem: array(array("Pakete", "Aktion", "IP"), array(300, "ACCEPT", "0.0.0.0/0"))
+ *  Wird dem: array(array("Pakete" => 300, "Aktion" => "ACCEPT", "IP" => "0.0.0.0/0"))
+ */
 function structureArray($array): array
 {
     $chain = null;
@@ -96,12 +138,29 @@ function structureArray($array): array
                     continue;
                 }
 
-                // TODO: Keep keys which get removed (Add as extra)
+                $removed = array();
                 if (count($keys) !== count($rule)) {
+                    $removed = array_chunk($rule, count($keys), true)[1];
                     $rule = array_slice($rule, 0, count($keys));
                 }
 
-                $value[$chain][] = array_combine($keys, $rule);
+                $extraKeys = array();
+                if (count($removed)) {
+                    foreach ($removed as $column) {
+                        $matches = array();
+                        if (preg_match('(NEW|RELATED|ESTABLISHED)', $column, $matches)) {
+                            array_push($extraKeys, "state");
+                            array_push($rule, $column);
+                        }
+
+                        if (preg_match('/\/\* (.*?) \*\//', $column, $matches)) {
+                            array_push($extraKeys, "comment");
+                            array_push($rule, $matches[1]);
+                        }
+                    }
+                }
+
+                $value[$chain][] = array_combine(array_merge($keys, $extraKeys), $rule);
                 unset($value[$ruleID]);
             }
         }
@@ -109,21 +168,30 @@ function structureArray($array): array
     return $array;
 }
 
-//while (sleep(2) !== null) {
+/*
+ * Autor: Bernardo de Oliveira
+ *
+ * Holt die Regeln von der Firewall und speichert alle Tabellen in einem Array
+ * Löscht alle leeren Elemente aus dem Array
+ * Sortiert die Daten
+ * Strukturiert die Daten neu
+ * Speichert diese ab
+ */
+while (sleep(2) !== null) {
     $raw = shell_exec("sudo iptables -t raw -L -n -v");
     $mangle = shell_exec("sudo iptables -t mangle -L -n -v");
+    $nat = shell_exec("sudo iptables -t nat -L -n -v");
     $filter = shell_exec("sudo iptables -t filter -L -n -v");
 
     $data = array();
     $data["raw"] = explode("\n", $raw);
     $data["mangle"] = explode("\n", $mangle);
+    $data["nat"] = explode("\n", $nat);
     $data["filter"] = explode("\n", $filter);
 
     $data = clearArray($data);
-    sort_recursive($data, SORT_NUMERIC);
+    sort_recursive($data);
     $data = structureArray($data);
 
-    print_r($data["raw"]);
-
     file_put_contents(__DIR__ . "/../db/firewall.json", json_encode($data));
-//}
+}
