d77d747ed3429ef82b4fcf2cc509243a6f84dd18
bernardo <bernardo@bernardo.fm>
2022-02-28 14:03:16 +0100

    Updated library which fixed get_class error

--- php/start.sh
+++ php/start.sh
@@ -1,4 +1,5 @@
 composer install --working-dir=/var/www/html/system/
+composer update --working-dir=/var/www/html/system/
 chown -R nginx:nginx /var/www/html
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
