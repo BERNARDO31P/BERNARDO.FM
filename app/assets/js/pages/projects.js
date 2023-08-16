215f18089cb855205578e6c1a3fa57a54c16e895
Bernardo de Oliveira <bernardo@bernardo.fm>
2023-08-16 14:54:07 +0200

    Fixed projects page

--- app/assets/js/pages/projects.js
+++ app/assets/js/pages/projects.js
@@ -10,15 +10,9 @@ window["projects"] = () => {
     let cards = document.createElement("div");
     cards.classList.add("cards");
 
-    let pageCount = getGetParameter(location.href,"p") ?? 1;
-    location.href = setGetParameter(location.href, "p", pageCount);
-
     let data = tryParseJSON(httpGet(object.dataset.url));
     if (data && data.length > 0) {
-        let startIndex = (pageCount - 1) * 10;
-        let paged = data.slice(startIndex, startIndex + 10);
-
-        for (let entry of paged) {
+        for (let entry of data) {
             let card = document.createElement("div");
             card.classList.add("card");
 
@@ -32,13 +26,12 @@ window["projects"] = () => {
 
             let github = document.createElement("a");
             github.textContent = "View on GitHub";
-            github.href = entry.html_url;
+            github.href = entry["html_url"];
             github.target = "_blank";
             github.classList.add("button");
             github.classList.add("shadow");
+
             card.appendChild(github);
-
-
             cards.appendChild(card);
         }
     }
