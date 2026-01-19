d5b1ed4cbe9b9dd20f0de21248e3c7ecbb1b4ace
Bernardo de Oliveira <bernardo@bernardo.fm>
2026-01-19 23:46:37 +0100

    Cleanup

--- app/assets/js/tools.js
+++ app/assets/js/tools.js
@@ -1614,8 +1614,6 @@ function addEvents(player) {
         if (e.detail.set) {
             nextPartIndex = e.detail.index
 
-            console.log(e.detail);
-
             if (player.isPlaying()) {
                 player.queueTrack(nextPartIndex);
             } else {
